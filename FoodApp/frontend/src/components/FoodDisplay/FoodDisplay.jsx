4082d35efba88a5aadb8304c300039423cacb4ae
Renzaki <renzakithakuri@gmail.com>
2024-03-26 13:49:09 +0545

    adding logic to additems to cart

--- FoodApp/frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ FoodApp/frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,22 +3,23 @@ import { useContext } from "react";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
   const { food_list } = useContext(StoreContext);
   return (
     <div className="mx-[200px] mt-1">
       <h2 className="font-bold text-3xl ">Top dishes near you</h2>
-      <div className="grid grid-rows-8 grid-flow-col gap-5">
-        {food_list.map((item, index) => (
-          <FoodItem
+      <div className="grid grid-rows-8 grid-flow-col gap-5 w-[350px]  ">
+        {food_list.map((item, index) => {
+          if(category === "All" || category === item.category){
+          return <FoodItem 
             key={index}
             id={item._id}
             name={item.name}
             description={item.description}
             price={item.price}
             image={item.image}
-          />
-        ))}
+          />;}
+        })}
       </div>
     </div>
   );
